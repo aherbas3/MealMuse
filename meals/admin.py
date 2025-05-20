92ded7a19ef0e966a549e4379236fb76f3a31704
aherbas3 <abigailrominaherbas@gmail.com>
2025-05-20 18:24:54 -0400

    register the meal models

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import GroceryList, Recipe, Ingredient
 
 # Register your models here.
+admin.site.register(GroceryList)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
